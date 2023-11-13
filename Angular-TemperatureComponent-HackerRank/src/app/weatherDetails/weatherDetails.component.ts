7ce53619be4cd4b5ba6d9b2893f88e29f5ddbd05
I Wayan Wahyu Surya Putra <146698039+WahyuSuryaPutra@users.noreply.github.com>
2023-11-13 22:42:14 +0800

    Add files via upload

---
+++ Angular-TemperatureComponent-HackerRank/src/app/weatherDetails/weatherDetails.component.ts
@@ -0,0 +1,29 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'weather-details',
+  templateUrl: './weatherDetails.component.html',
+  styleUrls: ['./weatherDetails.component.scss']
+})
+
+export class WeatherDetails implements OnInit {
+  @Input() weatherData: data[];
+
+  searchTerm = "";
+  searchedData?: data = undefined;
+
+  ngOnInit() {}
+
+  handleChange(value: string | null) {
+    this.searchedData = this.weatherData.find(
+      ({ name }) => name.toLowerCase() === value.toLowerCase()
+    );
+  }
+}
+
+interface data {
+  name: string;
+  temperature: string;
+  wind: string;
+  humidity: string;
+}
